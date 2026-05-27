ba16ebf0776a18f8c3b0a4ed87e7c9cd98bb6e0e
Mateusz Switala <mswitala@redhat.com>
2026-05-27 12:12:52 +0200

    fix __init__ file in automl subcategory
    Signed-off-by: Mateusz Switala <mswitala@redhat.com>

--- components/training/automl/__init__.py
+++ components/training/automl/__init__.py
@@ -4,15 +4,11 @@ if os.environ.get("_KFP_RUNTIME", "false") != "true":
     from . import (
         autogluon_leaderboard_evaluation,
         autogluon_models_training,
-        autogluon_timeseries_leaderboard_evaluation,
-        autogluon_timeseries_models_full_refit,
-        autogluon_timeseries_models_selection,
+        autogluon_timeseries_models_training,
     )
 
     __all__ = [
         "autogluon_leaderboard_evaluation",
         "autogluon_models_training",
-        "autogluon_timeseries_leaderboard_evaluation",
-        "autogluon_timeseries_models_full_refit",
-        "autogluon_timeseries_models_selection",
+        "autogluon_timeseries_models_training",
     ]
